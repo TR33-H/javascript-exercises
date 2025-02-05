664ef9744eb807f85f1f5971b4389828e6a359a9
TR33-H <demetriehayes@proton.me>
2025-02-05 07:49:32 -0600

    Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  fahrenheit -=32;
+  fahrenheit /= 1.8;
+  if(Number.isInteger(fahrenheit)){
+    return fahrenheit;
+  }else{
+    fahrenheit = Math.round(fahrenheit * 10) / 10;
+    return fahrenheit;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  celsius *= 1.8;
+  celsius += 32;
+  if(Number.isInteger(celsius)){
+    return celsius;
+  }else{
+    celsius = Math.round(celsius * 10) / 10;
+    return celsius;
+  }
 };
 
 // Do not edit below this line

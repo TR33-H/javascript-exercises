cf159fb3eaba0623b665f179412b687ef3273f35
TR33-H <demetriehayes@proton.me>
2025-02-05 07:26:50 -0600

    Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let larger;
+    let smaller;
+    let sum = 0;
+    if(num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    }
+    if(num1 > num2){
+        larger = num1;
+        smaller = num2;
+    } else{
+        larger = num2;
+        smaller = num1;
+    }
 
+    for(let i = larger; i >= smaller; i--){
+        sum += i; 
+    }
+    return sum;
 };
 
 // Do not edit below this line

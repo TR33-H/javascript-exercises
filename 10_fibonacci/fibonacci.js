305dcd1cfec356301229c91df8f59f2ea845c21a
TR33-H <demetriehayes@proton.me>
2025-03-04 10:48:23 -0600

    exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num, sequence = [1, 1]) {
+    if(num < 0){
+        return "OOPS";
+    }
+    if(num == 0){
+        return 0;
+    }
+    if(num > 2){
+        let sum = sequence.reduce((total, currentItem) =>{
+            return total + currentItem;
+        });
+        sequence.shift();   //removes first num
+        sequence.push(sum); //adds sum to end of array
+        let newnum = num - 1;
+        return fibonacci(newnum, sequence);
+    }
+    return sequence.pop();
+    
 };
 
 // Do not edit below this line

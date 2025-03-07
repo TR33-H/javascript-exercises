039e11e5d863ecca8242d7679a0a2efc9b801046
TR33-H <demetriehayes@proton.me>
2025-03-07 07:45:02 -0600

    exercise 11 & 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = 0;
+    let oldestName = "nobody";
+    for(peeps in people){
+        
+       
+        if(people[peeps].yearOfDeath == undefined){
+            let d = new Date();
+            let year = d.getFullYear();
+            people[peeps].yearOfDeath = year;
+        }
+        let age = people[peeps].yearOfDeath - people[peeps].yearOfBirth;
+        
+        if(age > oldest){
+            oldest = age;
+            oldestName = people[peeps];
+            
+        }
+        
+    }
+    
+    return oldestName;
 };
 
 // Do not edit below this line
